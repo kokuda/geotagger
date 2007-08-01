d9008fff2a08fad01413b218fd9bfdc103897f11
kokuda <kokuda@94a6b7f9-8936-0410-a1ab-05fe027bf721>
2007-08-01 08:05:00 +0000

    Added GPL Copyright notices to each source file and a COPYING file containing the GPL V3.  Also updated the copyright properties of the project.

--- trunk/Geotagger/Properties/AssemblyInfo.cs
+++ trunk/Geotagger/Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using System.Reflection;
+﻿//////////////////////////////////////////////////////////////////////////////
+//
+//    This file is part of Geotagger: A tool for geotagging photographs
+//    Copyright (C) 2007  Kaz Okuda (http://notions.okuda.ca)
+//
+//    Geotagger is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +26,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Geotagger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool for Geotagging photographs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Geotagger")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright ©  2007 Kaz Okuda")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
